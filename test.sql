f4b2739c9f4451b67596c347e1c63ad5258e88ff
13955952671 <892714612@qq.com>
2019-04-25 17:34:45 +0800

    init files

---
+++ test.sql
@@ -0,0 +1,7 @@
+prompt Importing table mk_codename_dict...
+set feedback off
+set define off
+insert into mk_codename_dict (TABLE_EN_NAME, TABLE_CN_NAME, FIELD_EN_NAME, FIELD_CN_NAME, FIELD_VALUE, FIELD_DESC, FIELD_URL, ASSI_TABLENNAME, ASSI_FIELDENNAME, ASSI_FIELDVALUE, PROVINCE_GROUP)
+values ('mk_batchSmsSendPort_dict', '批量短信下发端口', 'sendPort', '端口号', '10086021', '10086021', null, null, null, null, '10017');
+
+prompt Done.
